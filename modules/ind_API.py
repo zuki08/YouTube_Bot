738525536f8554bac1d6e9a273b06638294ccb13
Jeff L <jjl01332@gmail.com>
2024-01-07 11:16:59 -0500

    Postgres Reactivated. Voice Channel Link to be Implemented

--- modules/ind_API.py
+++ modules/ind_API.py
@@ -11,18 +11,19 @@ def search(query):
     cur = leConn.cursor()
     query = query.replace(" ", "%")
     # print(query)
-    q = f"SELECT DISTINCT * FROM info WHERE Lower(title) like '%{query}%';"
+    q = f"SELECT DISTINCT * FROM info WHERE title like '%{query}%';"
     # print(q)
     cur.execute(q)
     res = cur.fetchall()
+    print(res)
     if(len(res) == 0):
         data = yt.main(query)
         # data = json.loads(data)
-        # print(data["items"][0])
+        print(data["items"][0])
         data = data['items'][0]
-        # q = f"INSERT INTO info VALUES(nextval('inc'), '{data['snippet']['title']}', '{data['id']['videoId']}')"
-        # cur.execute(q)
-        # leConn.commit()
+        q = f"INSERT INTO info VALUES(nextval('inc'), '{data['snippet']['title']}', '{data['id']['videoId']}')"
+        cur.execute(q)
+        leConn.commit()
         return f"https://www.youtube.com/watch?v={data['id']['videoId']}"
     else:
         return f"https://www.youtube.com/watch?v={res[0][2]}"
